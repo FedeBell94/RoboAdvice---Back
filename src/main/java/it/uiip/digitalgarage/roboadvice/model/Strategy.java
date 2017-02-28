2017f483dd152bb672ab29ff65b82f514e6245ae
Simone Norcini <simone.norcini@gmail.com>
2017-02-28 14:57:29 +0100

    Bean entities

---
+++ src/main/java/it/uiip/digitalgarage/roboadvice/model/Strategy.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.uiip.digitalgarage.roboadvice.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.sql.Date;
+
+/**
+ *
+ * @author Simone
+ */
+public class Strategy implements Serializable {
+
+    private int id, userId, assetClassId;
+    private BigDecimal percentage;
+    private boolean active;
+    private Date startingDate;
+
+    public Strategy() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getAssetClassId() {
+        return assetClassId;
+    }
+
+    public void setAssetClassId(int assetClassId) {
+        this.assetClassId = assetClassId;
+    }
+
+    public BigDecimal getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(BigDecimal percentage) {
+        this.percentage = percentage;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public Date getStartingDate() {
+        return startingDate;
+    }
+
+    public void setStartingDate(Date startingDate) {
+        this.startingDate = startingDate;
+    }
+
+}
