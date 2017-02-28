2b04c9ae0da51c2f28dd8a42e11ba7386dae4a56
Simone Norcini <simone.norcini@gmail.com>
2017-02-28 15:53:00 +0100

    Login and SignUp first step

--- src/main/java/it/uiip/digitalgarage/roboadvice/controller/Application.java
+++ src/main/java/it/uiip/digitalgarage/roboadvice/controller/Application.java
@@ -1,4 +1,4 @@
-package hello;
+package it.uiip.digitalgarage.roboadvice.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
