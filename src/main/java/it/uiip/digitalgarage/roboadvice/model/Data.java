443e0a67f8f20d33184cdc5d129662b71671926f
Simone Norcini <simone.norcini@gmail.com>
2017-02-28 14:18:30 +0100

    bean entities

---
+++ src/main/java/it/uiip/digitalgarage/roboadvice/model/Data.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.uiip.digitalgarage.roboadvice.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.sql.Date;
+
+/**
+ *
+ * @author Simone
+ */
+public class Data implements Serializable {
+
+    private int id, assetId;
+    private Date date;
+    private BigDecimal value;
+
+    public Data() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAssetId() {
+        return assetId;
+    }
+
+    public void setAssetId(int assetId) {
+        this.assetId = assetId;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+
+}
---
+++ src/main/java/it/uiip/digitalgarage/roboadvice/model/Portfolio.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.uiip.digitalgarage.roboadvice.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.sql.Date;
+
+/**
+ *
+ * @author Simone
+ */
+public class Portfolio implements Serializable {
+
+    private int id, userId, assetClassId, assetId;
+    private BigDecimal unit, value;
+    private Date date;
+
+    public Portfolio() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getAssetClassId() {
+        return assetClassId;
+    }
+
+    public void setAssetClassId(int assetClassId) {
+        this.assetClassId = assetClassId;
+    }
+
+    public int getAssetId() {
+        return assetId;
+    }
+
+    public void setAssetId(int assetId) {
+        this.assetId = assetId;
+    }
+
+    public BigDecimal getUnit() {
+        return unit;
+    }
+
+    public void setUnit(BigDecimal unit) {
+        this.unit = unit;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+}
