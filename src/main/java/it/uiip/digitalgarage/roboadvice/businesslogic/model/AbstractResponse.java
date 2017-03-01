534f802217875fe3f734f5c62952db4615e8a738
federico.bellini <federicobellini94@gmail.com>
2017-03-01 11:02:32 +0100

    modified generic response to send also the error string

--- src/main/java/it/uiip/digitalgarage/roboadvice/businesslogic/model/AbstractResponse.java
+++ src/main/java/it/uiip/digitalgarage/roboadvice/businesslogic/model/AbstractResponse.java
@@ -4,12 +4,15 @@ public abstract class AbstractResponse<T> {
 
 	private int response;
 	private int errorCode;
+	private String errorString;
 	private T data;
 
 	protected AbstractResponse(int response, ExchangeError exchangeError, T data) {
 		this.response = response;
 		this.errorCode = exchangeError == null ? ExchangeError.DEFAULT_ERROR_CODE.getErrorCode()
 				: exchangeError.getErrorCode();
+		this.errorString = exchangeError == null ? ExchangeError.DEFAULT_ERROR_CODE.getErrorString()
+				: exchangeError.getErrorString();
 		this.data = data;
 	}
 
@@ -20,6 +23,8 @@ public abstract class AbstractResponse<T> {
 	public int getErrorCode() {
 		return this.errorCode;
 	}
+
+	public String getErrorString(){ return this.errorString; }
 
 	public T getData() {
 		return this.data;
