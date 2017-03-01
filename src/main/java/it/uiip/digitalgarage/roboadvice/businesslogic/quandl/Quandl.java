885b46a7d55c5b9aa5fe38e99bc1823aec25ca54
Simone Norcini <simone.norcini@gmail.com>
2017-03-01 19:48:45 +0100

    quandl daily and history

--- src/main/java/it/uiip/digitalgarage/roboadvice/businesslogic/quandl/Quandl.java
+++ src/main/java/it/uiip/digitalgarage/roboadvice/businesslogic/quandl/Quandl.java
@@ -5,7 +5,10 @@ import it.uiip.digitalgarage.roboadvice.persistence.model.Data;
 import org.threeten.bp.*;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
+import java.sql.Date;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 
 /**
@@ -15,7 +18,7 @@ public class Quandl {
 
     //api key: ydS_4tVLnsPS_bxo3uvd
 
-    public static void callQuandl(String quandlKey,int quandlColumn,int startYear, int startMonth, int startDay) {
+    public static ArrayList<Data> callQuandl(String quandlKey, int quandlColumn, int startYear, int startMonth, int startDay) {
 
         QuandlSession session = QuandlSession.create("ydS_4tVLnsPS_bxo3uvd");
 
@@ -25,8 +28,8 @@ public class Quandl {
                                 QuandlCodeRequest.singleColumn(quandlKey, quandlColumn),
                                 QuandlCodeRequest.allColumns("DOE/RWTC")
                         )
-                        .withStartDate(LocalDate.of(startYear,startMonth,startDay))
-                        .withFrequency(Frequency.MONTHLY)
+                        .withStartDate(LocalDate.of(startYear, startMonth, startDay))
+                        .withFrequency(Frequency.DAILY)
                         .build());
         System.out.println("Header definition: " + tabularResultMulti.getHeaderDefinition());
         ArrayList<Data> res = new ArrayList<>();
@@ -34,15 +37,47 @@ public class Quandl {
             LocalDate date = row.getLocalDate("Date");
             Double value = row.getDouble("DOE/RWTC - Value");
             System.out.println("Value on date " + date + " was " + value);
-            //res.add();
+
+            if (value != null) {
+                res.add(Data.builder().date(new Date(date.getYear(), date.getMonthValue(), date.getDayOfMonth())).value(BigDecimal.valueOf(value)).build());
+
+            }
+
         }
+
+        return res;
     }
 
-    public static void main(String [] args)
-    {
-        callQuandl("WIKI/AAPL",0,2015,10,11);
+    public static Data callDailyQuandl(String quandlKey, int quandlColumn) {
+
+        QuandlSession session = QuandlSession.create("ydS_4tVLnsPS_bxo3uvd");
+
+        TabularResult tabularResultMulti = session.getDataSets(
+                MultiDataSetRequest.Builder
+                        .of(
+                                QuandlCodeRequest.singleColumn(quandlKey, quandlColumn),
+                                QuandlCodeRequest.allColumns("DOE/RWTC")
+                        )
+                        .build());
+        System.out.println("Header definition: " + tabularResultMulti.getHeaderDefinition());
+
+        for (final Row row : tabularResultMulti) {
+            LocalDate date = row.getLocalDate("Date");
+            Double value = row.getDouble("DOE/RWTC - Value");
+            System.out.println("Value on date " + date + " was " + value);
+
+            if (value != null) {
+                return Data.builder().date(new Date(Calendar.getInstance().getTime().getTime())).value(BigDecimal.valueOf(value)).build();
+            }
+
+        }
+        return null;
     }
 
+    /*public static void main(String[] args) {
+        callQuandl("WIKI/FB", 0, 2015, 5, 11);
+        //callDailyQuandl("WIKI/FB",4);
+    }*/
 
 
 }
--- src/main/java/it/uiip/digitalgarage/roboadvice/persistence/model/Data.java
+++ src/main/java/it/uiip/digitalgarage/roboadvice/persistence/model/Data.java
@@ -5,6 +5,9 @@
  */
 package it.uiip.digitalgarage.roboadvice.persistence.model;
 
+import lombok.*;
+
+import javax.persistence.*;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.sql.Date;
@@ -13,45 +16,33 @@ import java.sql.Date;
  *
  * @author Simone
  */
+
+@Entity
+@Table(name = "Data")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Data implements Serializable {
 
-    private int id, assetId;
+    @Id
+    @GeneratedValue
+    private int id;
+
+    /*@Column(name = "assetId", nullable = false)
+    private int assetId;*/
+
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name = "asset", referencedColumnName = "asset_id")
+    private Asset asset;
+
+    @Column(name = "date", nullable = false)
     private Date date;
+
+    @Column(name = "value", nullable = false)
     private BigDecimal value;
 
-    public Data() {
-    }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getAssetId() {
-        return assetId;
-    }
-
-    public void setAssetId(int assetId) {
-        this.assetId = assetId;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    public BigDecimal getValue() {
-        return value;
-    }
-
-    public void setValue(BigDecimal value) {
-        this.value = value;
-    }
 
 }
