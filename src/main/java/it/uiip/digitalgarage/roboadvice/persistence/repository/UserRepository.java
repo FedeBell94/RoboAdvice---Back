e939d6365ce832fb5e993f7a79a35515dafe0c56
federico.bellini <federicobellini94@gmail.com>
2017-03-01 17:24:28 +0100

    changed repositories with Paging and sorting repositories

--- src/main/java/it/uiip/digitalgarage/roboadvice/persistence/repository/UserRepository.java
+++ src/main/java/it/uiip/digitalgarage/roboadvice/persistence/repository/UserRepository.java
@@ -4,12 +4,13 @@ import it.uiip.digitalgarage.roboadvice.persistence.model.AssetClass;
 import it.uiip.digitalgarage.roboadvice.persistence.model.User;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
 /**
  * Created by feder on 28/02/2017.
  */
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
 
     User findByEmail(String email);
 }
