bc17292b81b71bd185740fc42a93cc3cf372d0a1
federico.bellini <federicobellini94@gmail.com>
2017-03-02 11:57:08 +0100

    added possibility to populate database during first generation with default data taken from json object files -- Still work in progress

--- src/main/java/it/uiip/digitalgarage/roboadvice/Application.java
+++ src/main/java/it/uiip/digitalgarage/roboadvice/Application.java
@@ -1,12 +1,46 @@
 package it.uiip.digitalgarage.roboadvice;
 
+import it.uiip.digitalgarage.roboadvice.persistence.repository.AssetClassRepository;
+import it.uiip.digitalgarage.roboadvice.utils.Logger;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+import java.io.FileReader;
+import java.io.Reader;
+import java.util.List;
+
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Autowired
+    private AssetClassRepository assetClassRepository;
+
+    @PostConstruct
+    public void initDatabaseDefaultValues() throws Exception {
+        Logger.debug(Application.class, "Database default values checking and inserting");
+        //assetClassRepository.save(AssetClass.builder().name("prova").build());
+
+        JSONParser jsonParser = new JSONParser();
+        ClassLoader classLoader = getClass().getClassLoader();
+        Reader defaultAssetClassReader = new FileReader(classLoader.getResource("DefaultAssetClass.json").getPath());
+        try {
+            List<JSONObject> assetClassList = (List<JSONObject>) jsonParser.parse(defaultAssetClassReader);
+
+            for(JSONObject curr : assetClassList){
+                System.out.println((String)curr.get("name"));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
