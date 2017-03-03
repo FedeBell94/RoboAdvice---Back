81ab522f59db956e10df0f88778306b5ca434ef2
federico.bellini <federicobellini94@gmail.com>
2017-03-03 14:51:11 +0100

    updateStrategy rest api now working correctly

--- src/main/java/it/uiip/digitalgarage/roboadvice/businesslogic/controller/StrategyRESTController.java
+++ src/main/java/it/uiip/digitalgarage/roboadvice/businesslogic/controller/StrategyRESTController.java
@@ -5,10 +5,13 @@ import it.uiip.digitalgarage.roboadvice.businesslogic.model.AbstractResponse;
 import it.uiip.digitalgarage.roboadvice.businesslogic.model.ErrorResponse;
 import it.uiip.digitalgarage.roboadvice.businesslogic.model.ExchangeError;
 import it.uiip.digitalgarage.roboadvice.businesslogic.model.SuccessResponse;
+import it.uiip.digitalgarage.roboadvice.persistence.model.Asset;
 import it.uiip.digitalgarage.roboadvice.persistence.model.AssetClass;
+import it.uiip.digitalgarage.roboadvice.persistence.model.Strategy;
 import it.uiip.digitalgarage.roboadvice.persistence.model.User;
 import it.uiip.digitalgarage.roboadvice.persistence.repository.AssetClassRepository;
 import it.uiip.digitalgarage.roboadvice.persistence.repository.StrategyRepository;
+import it.uiip.digitalgarage.roboadvice.persistence.repository.UserRepository;
 import it.uiip.digitalgarage.roboadvice.utils.AuthProvider;
 import it.uiip.digitalgarage.roboadvice.utils.Logger;
 import org.json.simple.JSONObject;
@@ -16,6 +19,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.util.Calendar;
 import java.util.Iterator;
 import java.util.List;
 
@@ -29,6 +35,9 @@ public class StrategyRESTController {
     @Autowired
     private AssetClassRepository assetClassRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @CrossOrigin(origins = "*")
     @RequestMapping(value = "/updateStrategy", method = RequestMethod.POST)
     public @ResponseBody AbstractResponse updateStrategy(@RequestBody JSONObject strategyInput, HttpServletRequest request) {
@@ -38,12 +47,26 @@ public class StrategyRESTController {
             Logger.debug(StrategyRESTController.class, "Request with wrong user token.");
             return new ErrorResponse(ExchangeError.SECURITY_ERROR);
         }
+        User user = userRepository.findOne(userId);
 
+        // Disable the previous active strategy
+        List<Strategy> previousActive = strategyRepository.findByUserAndActiveTrue(user);
+        for(Strategy curr: previousActive){
+            strategyRepository.disactiveStrategy(curr.getId());
+        }
 
+        // Insert new strategy
         Iterable<AssetClass> it = assetClassRepository.findAll();
         for(AssetClass curr : it){
-            Logger.error(StrategyRESTController.class, (String)strategyInput.get(String.valueOf(curr.getId())));
-            // TODO persistence!!!
+            BigDecimal percentage = new BigDecimal((String) strategyInput.get(String.valueOf(curr.getId())));
+            Strategy newStrategy = Strategy.builder()
+                    .user(user)
+                    .assetClass(curr)
+                    .percentage(percentage)
+                    .active(true)
+                    .startingDate(new Date(Calendar.getInstance().getTimeInMillis())).build();
+            strategyRepository.save(newStrategy);
+            Logger.debug(StrategyRESTController.class, "Insert strategy " + newStrategy);
         }
         return new SuccessResponse<>(null);
     }
