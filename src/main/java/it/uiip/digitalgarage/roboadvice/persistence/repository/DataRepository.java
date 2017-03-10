88c9c4e9a75dd63693cac1a759c304033e5dde80
Simone Norcini <simone.norcini@gmail.com>
2017-03-10 10:34:17 +0100

    /asset value api in the last year

--- src/main/java/it/uiip/digitalgarage/roboadvice/persistence/repository/DataRepository.java
+++ src/main/java/it/uiip/digitalgarage/roboadvice/persistence/repository/DataRepository.java
@@ -4,6 +4,8 @@ import it.uiip.digitalgarage.roboadvice.persistence.model.Asset;
 import it.uiip.digitalgarage.roboadvice.persistence.model.Data;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 /**
  * Created by Simone on 02/03/2017.
@@ -13,4 +15,6 @@ public interface DataRepository extends JpaRepository<Data, Integer> {
     Data findByAssetAndDate(Asset asset, java.sql.Date date);
 
     Data findFirst1ByAssetOrderByDateDesc(Asset asset);
+
+    List<Data> findFirst360ByAssetOrderByDateAsc(Asset asset);
 }
