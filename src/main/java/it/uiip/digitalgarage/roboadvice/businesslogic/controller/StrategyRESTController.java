ab191716e7a25ecef9de1e395e5ce8814675e3ac
federico.bellini <federicobellini94@gmail.com>
2017-03-12 16:50:12 +0100

    commented code of strategy controller

--- src/main/java/it/uiip/digitalgarage/roboadvice/businesslogic/controller/StrategyRESTController.java
+++ src/main/java/it/uiip/digitalgarage/roboadvice/businesslogic/controller/StrategyRESTController.java
@@ -17,6 +17,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.Date;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.LinkedList;
 import java.util.List;
@@ -25,6 +26,7 @@ import java.util.List;
  * Class used to create all the API rest used to manage the {@link Strategy}.
  */
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping(value = "securedApi")
 public class StrategyRESTController {
 
@@ -39,34 +41,57 @@ public class StrategyRESTController {
 
     private static final Log LOGGER = LogFactory.getLog(StrategyRESTController.class);
 
-    @CrossOrigin(origins = "*")
+    /**
+     * Retrieve the last active strategy for the caller {@link User}.
+     *
+     * @param authentication
+     *         Represents the token for an authentication request or for an authenticated {@link User}.
+     *
+     * @return The last active {@link StrategyDTO} for the user, null in case the user has not a strategy set.
+     */
     @RequestMapping(value = "/strategy", method = RequestMethod.GET)
-    public @ResponseBody AbstractResponse getStrategy(Authentication authentication) {
+    public @ResponseBody AbstractResponse getActiveStrategy(Authentication authentication) {
         User user = userRepository.findByUsername(authentication.getName());
-        // Retrieve the strategies
+
+        // Retrieve the strategy
         List<Strategy> strategy = strategyRepository.findByUserAndActiveTrue(user);
         List<StrategyDTO> strategyDTO = new LinkedList<>();
         for (Strategy curr : strategy) {
             strategyDTO.add(new StrategyDTO(curr));
         }
-        LOGGER.debug("Get strategy API called.");
+        LOGGER.debug("User: " + user.getUsername() + " - Get strategy API called.");
         return new SuccessResponse<>(strategyDTO);
     }
 
-    @CrossOrigin(origins = "*")
+    /**
+     * Insert the strategy passed if the {@link User} is new, or update the strategy if the {@link User} is not new.
+     *
+     * @param strategyInput
+     *         The new strategy to insert into the database.
+     * @param authentication
+     *         Represents the token for an authentication request or for an authenticated {@link User}.
+     *
+     * @return An empty {@link SuccessResponse}.
+     */
     @RequestMapping(value = "/strategy", method = RequestMethod.POST)
     public @ResponseBody AbstractResponse updateStrategy(@RequestBody List<StrategyDTO> strategyInput,
                                                          Authentication authentication) {
         User user = userRepository.findByUsername(authentication.getName());
+        // Set user as not new if he was new
+        if (user.isNewUser()) {
+            user.setNewUser(false);
+            userRepository.save(user);
+        }
 
-        // Disable the previous active strategy
+        // Disable the previous active strategy if there is one
         List<Strategy> previousActive = strategyRepository.findByUserAndActiveTrue(user);
         for (Strategy curr : previousActive) {
             curr.setActive(false);
             strategyRepository.save(curr);
         }
 
-        // Insert new strategy
+        // Insert the new strategy
+        List<Strategy> insertStrategy = new ArrayList<>();
         for (StrategyDTO curr : strategyInput) {
             AssetClass assetClass = assetClassRepository.findOne(curr.getAssetClassId());
             Strategy newStrategy = Strategy.builder()
@@ -75,15 +100,10 @@ public class StrategyRESTController {
                     .percentage(curr.getPercentage())
                     .active(true)
                     .startingDate(new Date(Calendar.getInstance().getTimeInMillis())).build();
-            strategyRepository.save(newStrategy);
-            LOGGER.debug("Inserted strategy " + newStrategy);
+            insertStrategy.add(newStrategy);
+            LOGGER.debug("User: " + user.getUsername() + " - Inserted strategy " + newStrategy);
         }
-
-        // Set user as not new
-        if(user.isNewUser()){
-            user.setNewUser(false);
-            userRepository.save(user);
-        }
+        strategyRepository.save(insertStrategy);
 
         return new SuccessResponse<>(null);
     }
