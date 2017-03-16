b28514200eb0e600ea841ec19c1e86f42332fdde
Simone Norcini <simone.norcini@gmail.com>
2017-03-16 17:24:51 +0100

    UserRESTController unit test complete

--- src/main/java/it/uiip/digitalgarage/roboadvice/businesslogic/controller/UserRESTController.java
+++ src/main/java/it/uiip/digitalgarage/roboadvice/businesslogic/controller/UserRESTController.java
@@ -47,13 +47,13 @@ public class UserRESTController {
     /**
      * Perform the login of the user into the platform.
      *
-     * @param authentication
-     *         Represents the token for an authentication request or for an authenticated {@link User}.
-     *
+     * @param authentication Represents the token for an authentication request or for an authenticated {@link User}.
      * @return A {@link SuccessResponse} containing the {@link UserDTO} who asked for the login.
      */
     @RequestMapping(value = "/loginUser", method = RequestMethod.POST)
-    public @ResponseBody AbstractResponse loginUser(Authentication authentication) {
+    public
+    @ResponseBody
+    AbstractResponse loginUser(Authentication authentication) {
         User user = userRepository.findByUsername(authentication.getName());
         LOGGER.debug("User " + user.getUsername() + " just logged in.");
         return new SuccessResponse<>(modelMapper.map(user, UserDTO.class));
@@ -62,18 +62,16 @@ public class UserRESTController {
     /**
      * Performs the logout of the caller user fom the platform.
      *
-     * @param authentication
-     *         Represents the token for an authentication request or for an authenticated {@link User}.
-     * @param request
-     *         The {@link HttpServletRequest} of the servlet.
-     * @param response
-     *         The {@link HttpServletResponse} of the servlet.
-     *
+     * @param authentication Represents the token for an authentication request or for an authenticated {@link User}.
+     * @param request        The {@link HttpServletRequest} of the servlet.
+     * @param response       The {@link HttpServletResponse} of the servlet.
      * @return An empty {@link SuccessResponse}.
      */
     @RequestMapping(value = "/logoutUser", method = RequestMethod.POST)
-    public @ResponseBody AbstractResponse logoutUser(Authentication authentication, HttpServletRequest request,
-                                                     HttpServletResponse response) {
+    public
+    @ResponseBody
+    AbstractResponse logoutUser(Authentication authentication, HttpServletRequest request,
+                                HttpServletResponse response) {
         new SecurityContextLogoutHandler().logout(request, response, authentication);
         SecurityContextHolder.getContext().setAuthentication(null);
         LOGGER.debug("User " + authentication.getName() + " just logged out.");
@@ -83,13 +81,13 @@ public class UserRESTController {
     /**
      * Returns to the caller his identity (as {@link User}).
      *
-     * @param authentication
-     *         Represents the token for an authentication request or for an authenticated {@link User}.
-     *
+     * @param authentication Represents the token for an authentication request or for an authenticated {@link User}.
      * @return Returns the {@link User} which has called this method.
      */
     @RequestMapping(value = "/tellMeWhoAmI", method = RequestMethod.GET)
-    public @ResponseBody AbstractResponse tellMeWhoAmI(Authentication authentication) {
+    public
+    @ResponseBody
+    AbstractResponse tellMeWhoAmI(Authentication authentication) {
         User user = userRepository.findByUsername(authentication.getName());
         LOGGER.debug("TellWhoAmI: " + user.getUsername());
         return new SuccessResponse<>(modelMapper.map(user, UserDTO.class));
@@ -98,16 +96,18 @@ public class UserRESTController {
     /**
      * Register the {@link User} in the platform. This API is not secured.
      *
-     * @param inputUser
-     *         The {@link UserDTO} user to register.
-     *
+     * @param inputUser The {@link UserDTO} user to register.
      * @return A {@link SuccessResponse} containing the user created, an {@link ErrorResponse} containing the error code
      * if something goes wrong during the registration.
      */
     @RequestMapping(value = "/registerUser", method = RequestMethod.POST)
-    public @ResponseBody AbstractResponse registerUser(@RequestBody UserDTO inputUser) {
+    public
+    @ResponseBody
+    AbstractResponse registerUser(@RequestBody UserDTO inputUser) {
+
         if (userRepository.findByUsername(inputUser.getUsername()) != null) {
             LOGGER.debug("Email already used for this user");
+
             return new ErrorResponse(ExchangeError.EMAIL_ALREADY_USED);
         }
 
@@ -121,7 +121,6 @@ public class UserRESTController {
                 .isNewUser(true)
                 .build();
         userRepository.save(user);
-
         LOGGER.debug("User " + inputUser.getUsername() + " registered successfully");
         return new SuccessResponse<>(modelMapper.map(user, UserDTO.class));
     }
--- src/test/java/it/uiip/digitalgarage/roboadvice/tests/UserRESTControllerTest.java
+++ src/test/java/it/uiip/digitalgarage/roboadvice/tests/UserRESTControllerTest.java
@@ -2,6 +2,8 @@ package it.uiip.digitalgarage.roboadvice.tests;
 
 import it.uiip.digitalgarage.roboadvice.businesslogic.controller.UserRESTController;
 import it.uiip.digitalgarage.roboadvice.businesslogic.model.dto.UserDTO;
+import it.uiip.digitalgarage.roboadvice.businesslogic.model.response.AbstractResponse;
+import it.uiip.digitalgarage.roboadvice.businesslogic.model.response.SuccessResponse;
 import it.uiip.digitalgarage.roboadvice.persistence.model.User;
 import it.uiip.digitalgarage.roboadvice.persistence.repository.UserRepository;
 import org.junit.Before;
@@ -22,6 +24,11 @@ import org.springframework.test.context.support.DependencyInjectionTestExecution
 import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 
 import javax.persistence.PersistenceContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.sql.Date;
+import java.time.LocalDate;
 
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.when;
@@ -51,23 +58,42 @@ public class UserRESTControllerTest {
     @Mock
     ModelMapper modelMapper;
 
+    @Mock
+    HttpServletRequest request;
+
+    @Mock
+    HttpServletResponse response;
+
 
     @Rule
     public MockitoRule mockitoRule = MockitoJUnit.rule();
 
     private UserRESTController userRESTController;
 
+    private UserDTO userDTO;
+
     @Before
     public void before(){
 
 
         User user = User.builder().id(1L).username("testUser").password("12345").nickname("testUser").build();
+        User userReg = User.builder().username("testNewUser")
+                .password("HASHED12345")
+                .nickname("testNewUser")
+                .registration(Date.valueOf(LocalDate.now()))
+                .enabled(true)
+                .isNewUser(true)
+                .build();
+
+        userDTO = UserDTO.builder().username("testNewUser").password("12345").nickname("testNewUser").isNewUser(true).build();
 
         userRESTController = new UserRESTController(modelMapper,userRepository,passwordEncoder);
         when(authentication.getName()).thenReturn("testUser");
         when(userRepository.findByUsername("testUser")).thenReturn(user);
-        when(modelMapper.map(user,UserDTO.class))
+        when(modelMapper.map(user, UserDTO.class))
                 .thenReturn(UserDTO.builder().username("testUser").nickname("testUser").build());
+        when(userRepository.findByUsername(userDTO.getUsername())).thenReturn(null);
+        when(passwordEncoder.encode("12345")).thenReturn("HASHED12345");
 
     }
 
@@ -75,6 +101,22 @@ public class UserRESTControllerTest {
     public void loginTest(){
         UserDTO user = (UserDTO) userRESTController.loginUser(authentication).getData();
         Boolean check = user.getUsername().equals("testUser");
+        assertTrue(check);
+    }
+
+    @Test
+    public void tellMeWhoAmITest(){
+        UserDTO user = (UserDTO) userRESTController.tellMeWhoAmI(authentication).getData();
+        Boolean check = user.getUsername().equals("testUser");
+        assertTrue(check);
+    }
+
+    @Test
+    public void registerUserTest(){
+
+        AbstractResponse response = userRESTController.registerUser(userDTO);
+        System.out.println(response.toJSONFormat());
+        Boolean check = response.getResponse() == 1;
         assertTrue(check);
     }
 
