054a5db9eaacbd0b33b09150cfde2e61b7d86f29
federico.bellini <federicobellini94@gmail.com>
2017-03-16 11:51:53 +0100

    mapped strategy entity to strategyDTO usign modelMapper

--- src/main/java/it/uiip/digitalgarage/roboadvice/businesslogic/model/dto/StrategyDTO.java
+++ src/main/java/it/uiip/digitalgarage/roboadvice/businesslogic/model/dto/StrategyDTO.java
@@ -1,6 +1,5 @@
 package it.uiip.digitalgarage.roboadvice.businesslogic.model.dto;
 
-import it.uiip.digitalgarage.roboadvice.persistence.model.Strategy;
 import lombok.*;
 
 import java.math.BigDecimal;
@@ -11,9 +10,4 @@ public class StrategyDTO  {
     private Long assetClassId;
 
     private BigDecimal percentage;
-
-    public StrategyDTO(Strategy strategy){
-        this.assetClassId = strategy.getAssetClass().getId();
-        this.percentage = strategy.getPercentage();
-    }
 }
