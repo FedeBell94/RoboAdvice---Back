86e30675bfe207dd5dca18642b074ca9abfe30c9
Simone Norcini <simone.norcini@gmail.com>
2017-03-20 17:09:05 +0100

    Custom Date Unit Test

--- src/main/java/it/uiip/digitalgarage/roboadvice/utils/CustomDate.java
+++ src/main/java/it/uiip/digitalgarage/roboadvice/utils/CustomDate.java
@@ -12,19 +12,19 @@ public class CustomDate {
     /*
      *    CONSTRUCTORS
      */
-    public CustomDate(LocalDate localDate){
+    public CustomDate(LocalDate localDate) {
         this.localDate = localDate;
     }
 
-    public CustomDate(Date date){
+    public CustomDate(Date date) {
         this.localDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
     }
 
-    public CustomDate(java.sql.Date date){
+    public CustomDate(java.sql.Date date) {
         this.localDate = date.toLocalDate();
     }
 
-    public CustomDate(String s){
+    public CustomDate(String s) {
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         this.localDate = LocalDate.parse(s, dateTimeFormatter);
     }
@@ -33,119 +33,109 @@ public class CustomDate {
     /*
      *    GETTERS
      */
-    public LocalDate getDateLocalDate(){
+    public LocalDate getDateLocalDate() {
         return toLocalDate(this.localDate);
     }
 
-    public Date getDateUtils(){
+    public Date getDateUtils() {
         return toDateUtils(this.localDate);
     }
 
-    public java.sql.Date getDateSql(){
+    public java.sql.Date getDateSql() {
         return toDateSql(this.localDate);
     }
 
-    public org.threeten.bp.LocalDate getDateBpLocalDate(){
+    public org.threeten.bp.LocalDate getDateBpLocalDate() {
         return org.threeten.bp.LocalDate.of(localDate.getYear(), localDate.getMonthValue(), localDate.getDayOfMonth());
     }
 
-
-    public LocalDate getYesterdayLocalDate(){
+    public LocalDate getYesterdayLocalDate() {
         return getDayFromLocalDate(-1);
     }
 
-    public Date getYesterdayUtils(){
+    public Date getYesterdayUtils() {
         return getDayFromUtils(-1);
     }
 
-    public java.sql.Date getYesterdaySql(){
+    public java.sql.Date getYesterdaySql() {
         return getDayFromSql(-1);
     }
 
-
-    public LocalDate getDayFromLocalDate(int days){
-        if(days > 0){
+    public LocalDate getDayFromLocalDate(int days) {
+        if (days > 0) {
             return this.localDate.plusDays(days);
         }
         return this.localDate.minusDays(-days);
     }
 
-    public Date getDayFromUtils(int days){
+    public Date getDayFromUtils(int days) {
         return toDateUtils(getDayFromLocalDate(days));
     }
 
-    public java.sql.Date getDayFromSql(int days){
+    public java.sql.Date getDayFromSql(int days) {
         return toDateSql(getDayFromLocalDate(days));
     }
 
-
-
-    public int getYear(){
+    public int getYear() {
         return this.localDate.getYear();
     }
 
-    public int getMonth(){
-        return  this.localDate.getMonthValue();
+    public int getMonth() {
+        return this.localDate.getMonthValue();
     }
 
-    public int getDay(){
+    public int getDay() {
         return this.localDate.getDayOfMonth();
     }
-
-
-
 
     /*
      *    UTILITIES
      */
-    public CustomDate moveOneDayForward(){
+    public CustomDate moveOneDayForward() {
         return moveForward(1);
     }
 
-    public CustomDate moveForward(int days){
+    public CustomDate moveForward(int days) {
         this.localDate = this.localDate.plusDays(days);
         return this;
     }
 
-
-    public int compareTo(LocalDate ld){
+    public int compareTo(LocalDate ld) {
         return this.localDate.compareTo(ld);
     }
 
-    public int compateTo(Date date){
+    public int compareTo(Date date) {
         return compareTo(fromUtil(date));
     }
 
-    public int compareTo(java.sql.Date date){
+    public int compareTo(java.sql.Date date) {
         return compareTo(fromSql(date));
     }
 
-    public int compareTo(CustomDate customDate){
+    public int compareTo(CustomDate customDate) {
         return compareTo(customDate.getDateLocalDate());
     }
-
-
 
     /*
      *    CONVERSION UTILS
      */
-    private LocalDate toLocalDate(LocalDate ld){
+    private LocalDate toLocalDate(LocalDate ld) {
         return LocalDate.of(ld.getYear(), ld.getMonth(), ld.getDayOfMonth());
     }
 
-    private Date toDateUtils(LocalDate ld){
+    private Date toDateUtils(LocalDate ld) {
         return Date.from(ld.atStartOfDay(ZoneId.systemDefault()).toInstant());
     }
 
-    private java.sql.Date toDateSql(LocalDate ld){
+    private java.sql.Date toDateSql(LocalDate ld) {
         return java.sql.Date.valueOf(ld);
     }
 
-    private LocalDate fromSql(java.sql.Date date){
+    private LocalDate fromSql(java.sql.Date date) {
         return date.toLocalDate();
     }
 
-    private LocalDate fromUtil(Date date){
+    private LocalDate fromUtil(Date date) {
         return date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
     }
 
--- src/test/java/it/uiip/digitalgarage/roboadvice/test/unitTests/CustomDateTests.java
+++ src/test/java/it/uiip/digitalgarage/roboadvice/test/unitTests/CustomDateTests.java
@@ -2,12 +2,18 @@ package it.uiip.digitalgarage.roboadvice.test.unitTests;
 
 import it.uiip.digitalgarage.roboadvice.utils.CustomDate;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+
+import java.sql.Date;
+import java.util.Calendar;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by Simone on 20/03/2017.
@@ -19,9 +25,129 @@ import org.springframework.test.context.transaction.TransactionalTestExecutionLi
         TransactionalTestExecutionListener.class})
 public class CustomDateTests {
 
-    @Before
-    public void before() {
+    private CustomDate customDate;
 
+    private Calendar calendar;
+
+    @Before
+    public void before(){
+
+        calendar = Calendar.getInstance();
 
     }
+
+    @Test
+    public void testLocalDate() {
+
+        customDate = new CustomDate(new Date(calendar.getTime().getTime()).toLocalDate());
+
+        Boolean check1 = customDate.getDateLocalDate().getClass().toString().equals("class java.time.LocalDate");
+
+        Boolean check2 = customDate.getDateUtils().getClass().toString().equals("class java.util.Date");
+
+        Boolean check3 = customDate.getDateSql().getClass().toString().equals("class java.sql.Date");
+
+        Boolean check4 = customDate.getDateBpLocalDate().getClass().toString().equals("class org.threeten.bp.LocalDate");
+
+        assertTrue(check1);
+
+        assertTrue(check2);
+
+        assertTrue(check3);
+
+        assertTrue(check4);
+
+        calendar.add(Calendar.DATE, -1);
+
+        Boolean check5 = customDate.getYesterdayLocalDate().equals(new Date(calendar.getTime().getTime()).toLocalDate());
+
+        assertTrue(check5);
+
+    }
+
+    @Test
+    public void testUtilDate(){
+
+        customDate = new CustomDate(calendar.getTime());
+
+        Boolean check1 = customDate.getDateLocalDate().getClass().toString().equals("class java.time.LocalDate");
+
+        Boolean check2 = customDate.getDateUtils().getClass().toString().equals("class java.util.Date");
+
+        Boolean check3 = customDate.getDateSql().getClass().toString().equals("class java.sql.Date");
+
+        Boolean check4 = customDate.getDateBpLocalDate().getClass().toString().equals("class org.threeten.bp.LocalDate");
+
+        assertTrue(check1);
+
+        assertTrue(check2);
+
+        assertTrue(check3);
+
+        assertTrue(check4);
+
+        calendar.add(Calendar.DATE, -1);
+
+        calendar.set(Calendar.HOUR, -12);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+
+        Boolean check5 = customDate.getYesterdayUtils().toString().equals(calendar.getTime().toString());
+
+        assertTrue(check5);
+
+    }
+
+    @Test
+    public void testSqlDate(){
+
+        customDate = new CustomDate(new java.sql.Date(calendar.getTime().getTime()));
+
+        Boolean check1 = customDate.getDateLocalDate().getClass().toString().equals("class java.time.LocalDate");
+
+        Boolean check2 = customDate.getDateUtils().getClass().toString().equals("class java.util.Date");
+
+        Boolean check3 = customDate.getDateSql().getClass().toString().equals("class java.sql.Date");
+
+        Boolean check4 = customDate.getDateBpLocalDate().getClass().toString().equals("class org.threeten.bp.LocalDate");
+
+        assertTrue(check1);
+
+        assertTrue(check2);
+
+        assertTrue(check3);
+
+        assertTrue(check4);
+
+        calendar.add(Calendar.DATE, -1);
+
+        Boolean check5 = customDate.getYesterdaySql().toString().equals(new java.sql.Date(calendar.getTime().getTime()).toString());
+
+        assertTrue(check5);
+
+    }
+
+    @Test
+    public void testParsing(){
+
+        customDate = new CustomDate("2017-03-20");
+
+        Boolean check1 = customDate.getDateLocalDate().getClass().toString().equals("class java.time.LocalDate");
+
+        Boolean check2 = customDate.getDateUtils().getClass().toString().equals("class java.util.Date");
+
+        Boolean check3 = customDate.getDateSql().getClass().toString().equals("class java.sql.Date");
+
+        Boolean check4 = customDate.getDateBpLocalDate().getClass().toString().equals("class org.threeten.bp.LocalDate");
+
+        assertTrue(check1);
+
+        assertTrue(check2);
+
+        assertTrue(check3);
+
+        assertTrue(check4);
+
+    }
+
 }
