5e5052aa15ed6aeeb6ec819dbc266edc108cee53
federico.bellini <federicobellini94@gmail.com>
2017-03-20 17:35:22 +0100

    handled exception in case all the date passed are in a wrong format

--- src/main/java/it/uiip/digitalgarage/roboadvice/businesslogic/exception/GlobalControllerExceptionHandler.java
+++ src/main/java/it/uiip/digitalgarage/roboadvice/businesslogic/exception/GlobalControllerExceptionHandler.java
@@ -4,19 +4,26 @@ import it.uiip.digitalgarage.roboadvice.businesslogic.model.response.AbstractRes
 import it.uiip.digitalgarage.roboadvice.businesslogic.model.response.ErrorResponse;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.core.convert.ConversionFailedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@ResponseStatus(HttpStatus.OK)
 class GlobalControllerExceptionHandler {
 
     private static final Log LOGGER = LogFactory.getLog(GlobalControllerExceptionHandler.class);
 
-    @ResponseStatus(HttpStatus.OK)
     @ExceptionHandler(BadRequestException.class)
     public AbstractResponse handleBadRequestException(BadRequestException e) {
+        LOGGER.error(e.getMessage());
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler(ConversionFailedException.class)
+    public AbstractResponse handleTypeMismatchE(ConversionFailedException e) {
         LOGGER.error(e.getMessage());
         return new ErrorResponse(e.getMessage());
     }
