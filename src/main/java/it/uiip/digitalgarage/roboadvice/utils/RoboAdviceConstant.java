8ecdb3bc8ab70cb3e6c833f8718ec048db518bb0
federico.bellini <federicobellini94@gmail.com>
2017-03-23 18:23:10 +0100

    Implemented algorithm to know when a portfolio rebalancing is needed. Still need to be implemented the real rebalancing algorithm

--- src/main/java/it/uiip/digitalgarage/roboadvice/utils/RoboAdviceConstant.java
+++ src/main/java/it/uiip/digitalgarage/roboadvice/utils/RoboAdviceConstant.java
@@ -8,6 +8,8 @@ public class RoboAdviceConstant {
 
     public static final CustomDate STARTING_DATA = new CustomDate("2014-04-30");
 
+    public static final BigDecimal RE_BALANCING_BOUNDARIES = BigDecimal.valueOf(1);
+
     private RoboAdviceConstant(){}
 
 }
